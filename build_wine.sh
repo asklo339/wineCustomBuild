8e42ddcc2cb41d79bc501132918422e5f8405376
asklo339 <davitysk@gmail.com>
2025-04-30 07:52:26 +0530

    Update build_wine.sh

--- build_wine.sh
+++ build_wine.sh
@@ -485,7 +485,7 @@ if [ "$TERMUX_GLIBC" = "true" ]; then
     echo "Applying path change patch"
     ## Proton is based on Wine 9.0 stable release so some of the updates
     ## for patches are not required.
-    patch -d wine -Np1 < "${scriptdir}"/pathfix.patch || {
+    patch -d wine -Np1 < "${scriptdir}"/pathfix-wine9.5.patch || {
         echo "Error: Failed to apply one or more patches."
         exit 1
     }
