e1ae1395c80cb8e236ef11d76de67f0545601c43
asklo339 <davitysk@gmail.com>
2025-05-03 08:17:47 +0530

    Update build_protonGE.sh

--- build_protonGE.sh
+++ build_protonGE.sh
@@ -96,7 +96,7 @@ export WINE_BUILD_OPTIONS="--disable-winemenubuilder --disable-win16 --enable-wi
 # A temporary directory where the Wine source code will be stored.
 # Do not set this variable to an existing non-empty directory!
 # This directory is removed and recreated on each script run.
-export BUILD_DIR=/home/runner/work/Wine-DarkOS-Builds/Wine-DarkOS-Builds/wine
+export BUILD_DIR="${HOME}"/build_wine
 
 # Implement a new WoW64 specific check which will change the way Wine is built.
 # New WoW64 builds will use a different bootstrap which require different
